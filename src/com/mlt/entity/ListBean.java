cc9ece2c2a3b7c4d8fc6525cdf267c6a2faf731f
malitao2017 <934921721@qq.com>
2017-09-30 16:03:59 +0800

    添加详细内容

---
+++ src/com/mlt/entity/ListBean.java
@@ -0,0 +1,23 @@
+package com.mlt.entity;
+
+import java.util.List;
+
+public class ListBean {
+    private String name;
+    private List list;
+    
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List getList() {
+		return list;
+	}
+	public void setList(List list) {
+		this.list = list;
+	}
+    
+}
--- src/com/mlt/test/JibxTest.java
+++ src/com/mlt/test/JibxTest.java
@@ -3,6 +3,8 @@ package com.mlt.test;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jibx.runtime.BindingDirectory;
 import org.jibx.runtime.IBindingFactory;
@@ -15,6 +17,7 @@ import org.junit.Test;
 
 import com.mlt.entity.Account;
 import com.mlt.entity.Birthday;
+import com.mlt.entity.ListBean;
 
 public class JibxTest {
     private IBindingFactory factory = null;
@@ -67,8 +70,10 @@ public class JibxTest {
         System.err.println(o);
     }
     
-    @Test
+//    @Test
     public void bean2XML() {
+    	System.out.println("解析普通对象");
+    	System.out.println("运行之前需要： 在 D:/work/eclipse/workspace-jee/tech38XMLJibx>下运行： java -cp bin;lib/jibx-bind.jar org.jibx.binding.Compile -v bind.xml");
         try {
             writer = new StringWriter();
             // marshal 编组
@@ -82,6 +87,43 @@ public class JibxTest {
             IUnmarshallingContext uctx = factory.createUnmarshallingContext();
             Account acc = (Account) uctx.unmarshalDocument(reader, null);
             fail(acc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void listBean2XML() {
+    	System.out.println("解析集合对象：在运行前，一定要将最先前运行的Account那个类的bind.xml文件的内容加入到现在这个bind.xml中，因为ListBean依赖了Account这个类。");
+        try {
+            ListBean listBean = new ListBean();
+            List<Account> list = new ArrayList<Account>();
+            list.add(bean);
+            bean = new Account();
+            bean.setAddress("china");
+            bean.setEmail("tom@125.com");
+            bean.setId(2);
+            bean.setName("tom");
+            Birthday day = new Birthday("2010-11-22");
+            bean.setBirthday(day);
+            list.add(bean);
+            listBean.setList(list);
+            
+            writer = new StringWriter();
+            factory = BindingDirectory.getFactory(ListBean.class);
+            // marshal 编组
+            IMarshallingContext mctx = factory.createMarshallingContext();
+            mctx.setIndent(2);
+            mctx.marshalDocument(listBean, "UTF-8", null, writer);
+            fail(writer);
+            
+            reader = new StringReader(writer.toString());
+            //unmarshal 解组
+            IUnmarshallingContext uctx = factory.createUnmarshallingContext();
+            listBean = (ListBean) uctx.unmarshalDocument(reader, null);
+            
+            fail(listBean.getList().get(0));
+            fail(listBean.getList().get(1));
         } catch (Exception e) {
             e.printStackTrace();
         }
